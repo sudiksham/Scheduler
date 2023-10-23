ed202ad168dcfb02b27a586a2f15ee96dae77969
Sudiksha Malhotra <malhotrasudiksha@gmail.com>
2023-10-22 17:57:28 -0700

    added destroy

--- scheduler.c
+++ scheduler.c
@@ -16,6 +16,8 @@
 #include "scheduler.h"
 #define MAX_THREADS 50
 
+#define STACK_SIZE 1048576
+
 
 /**
  * Needs:
@@ -38,130 +40,39 @@ struct thread{
          void *memory_;
          void *memory;
          }stack;
-         scheduler_fnc_t function;
-         void *arg;
+         
          struct thread* next;
          };
-
-struct thread* new_thread = NULL;
-struct thread* head = NULL;
-struct thread* current_thread = NULL;
-struct thread* prev_thread = NULL;
-struct thread* last = NULL;
-int num_threads = 0;
-
-
-
-
-int scheduler_create(scheduler_fnc_t fnc, void *arg) {
-      if (num_threads >= MAX_THREADS) {
-        fprintf(stderr, "Maximum number of threads reached\n");
-        return -1;
-    }
+ static struct {
+    struct thread* head;
+    struct thread* curr;
+    jmp_buf ctx;  //main program context
+    } state;
     
+struct thread* new_thread = NULL;
+
+int scheduler_create(scheduler_fnc_t fnc, void *arg) {   //1
+    
+    //initialize thread object
     new_thread = (struct thread*)malloc(sizeof(struct thread*));
     new_thread->status = STATUS_INIT;
     new_thread->next= NULL;
-    new_thread->function = fnc;
-    new_thread->arg = arg;
+    new_thread->stack->memory = (void*)malloc(STACK_SIZE);
     num_threads++;
-
-    if (head == NULL) {
-        head = new_thread;
-        current_thread = head;
+   
+    if (state.head == NULL) {
+        state.head = new_thread;
+        state.curr = state.head;
     } else {
-        last = head;
-        while (last->next!= NULL) {
-            last = last->next;
-        }/**
- * Tony Givargis
- * Copyright (C), 2023
- * University of California, Irvine
- *
- * CS 238P - Operating Systems
- * scheduler.c
- */
-
-#undef _FORTIFY_SOURCE
-
-#include <unistd.h>
-#include <signal.h>
-#include <setjmp.h>
-#include "system.h"
-#include "scheduler.h"
-#define MAX_THREADS 50
-
-
-/**
- * Needs:
- *   setjmp()
- *   longjmp()
- */
-
-/* research the above Needed API and design accordingly */
-
-
-struct thread{
-     jmp_buf ctx;
-     enum {
-         STATUS_INIT,
-         STATUS_RUNNING,
-         STATUS_SLEEPING,
-         STATUS_TERMINATED
-         }status;
-     struct {
-         void *memory_;
-         void *memory;
-         }stack;
-         scheduler_fnc_t function;
-         void *arg;
-         struct thread* next;
-         }thread;
-
-struct thread* new_thread = NULL;
-struct thread* head = NULL;
-struct thread* current_thread = NULL;
-struct thread* prev_thread = NULL;
-struct thread* last = NULL;
-int num_threads = 0;
-
-
-
-
-int scheduler_create(scheduler_fnc_t fnc, void *arg) {
-      if (num_threads >= MAX_THREADS) {
-        fprintf(stderr, "Maximum number of threads reached\n");
-        return -1;
-    }
-    
-    new_thread = (struct thread*)malloc(sizeof(struct thread*));
-    new_thread->status = STATUS_INIT;
-    new_thread->next= NULL;
-    new_thread->function = fnc;
-    new_thread->arg = arg;
-    num_threads++;
-
-    if (head == NULL) {
-        head = new_thread;
-        current_thread = head;
-    } else {
-        last = head;
-        while (last->next!= NULL) {
-            last = last->next;
+    struct thread* current = head;
+        while (current->next!= NULL) {
+            current = current->next;
         }
-        last->next = new_thread;
+        lcurrent->next = new_thread;
     }
-
-    if (setjmp(new_thread->ctx) == 0) {
-        fnc(arg);
-        new_thread->status = STATUS_TERMINATED;
-        scheduler_yield();
-    }
-    
-   return 0;
 }
 
-void scheduler_yield(void) {
+void scheduler_yield(void) { //1 
       if (current_thread != NULL) {
         current_thread->status = STATUS_SLEEPING;
         prev_thread = current_thread;
@@ -183,23 +94,11 @@ void scheduler_yield(void) {
     }
 }
 
-void scheduler_execute(void) {
-     if (current_thread != NULL) {
-        if (setjmp(current_thread->ctx) == 0) {
-            longjmp(current_thread->ctx, 1);
-        }
-        schedule();
-        destroy();
-        return 0;
-    }
-    return -1;
-}
-
 void destroy() {
-    if (current_thread != NULL) {
-        thread* prev_thread = NULL;
-        thread* current = head;
-        while (current != current_thread) {
+    if (state.curr!= NULL) {
+        struct thread* prev_thread = NULL;
+        struct thread* current = state.head;
+        while (current != state.curr) {
             prev_thread = current;
             current = current->next;
         }
@@ -209,59 +108,52 @@ void destroy() {
         } else {
             head = current->next;
         }
+        
+        FREE(current->stack->memory);
+        FREE(current->stack->memory_);
+        FREE(current);
 
-        free(current);
-        num_threads--;
-
-        if (current_thread == current) {
-            current_thread = NULL;
-        }
     }
-}
-
-void schedule(void) {
 }
 
 struct thread* thread_candidate(void) {
+  
+  if(state.head == NULL)
+      return NULL;
+      
+  struct thread* current = state.curr;
+  while(current!=NULL){
+      if(current->status == STATUS_INIT || current->status == STATUS_SLEEPING) {
+          return current;
+      }
+      if(current->next == NULL)
+         current = head;
+       else
+          current = current->next;
+          
+       if(current == state.curr && current-> status == STATUS_TERMINATED)
+         return NULL;
+  }
+  
+  return NULL;
+  }
 
-}
+void schedule(void) {
+ 
+  state.curr->status = STATUS_SLEEPING;
+  struct thread* candidate = thread_candidate();
+  
+  if(candidate == NULL)
+     return NULL;
      
-        last->next = new_thread;
-    }
-
-    if (setjmp(new_thread->ctx) == 0) {
-        fnc(arg);
-        new_thread->status = STATUS_TERMINATED;
-        scheduler_yield();
-    }
-    
-   return 0;
+   if(candidate->status == STATUS_SLEEPING){
+        state.curr = candidate;
+        candidate-> STATUS_RUNNING;
+        longjmp(candidate->ctx, 1);
+   }
+  
+   else {
+        
+   }
 }
 
-void scheduler_yield(void) {
-      if (current_thread != NULL) {
-        current_thread->status = STATUS_SLEEPING;
-        prev_thread = current_thread;
-        current_thread = current_thread->next;
-
-        while (current_thread == NULL || current_thread->status == STATUS_TERMINATED) {
-            if (current_thread == NULL) {
-                current_thread = head;
-            } else {
-                current_thread = current_thread->next;
-            }
-        }
-
-        current_thread->status = STATUS_RUNNING;
-
-        if (setjmp(prev_thread->ctx) == 0) {
-            longjmp(current_thread->ctx, 1);
-        }
-    }
-}
-
-void scheduler_execute(void) {
-
-
-}
-     
